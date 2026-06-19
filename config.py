d2c8293f399c0ffe763aacf756cf341cb27c696f
fcastautomation1-ux <fcast.automation1@gmail.com>
2026-06-19 16:14:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
 # --- Google Sheets ---
-SPREADSHEET_ID = '1NDp5gwAYsdj-tC4mNqSDECJ9eXwD6fG4zFK6vFcy17E' 
+SPREADSHEET_ID = '1GPGLZxGjKE4ICz89vvJWZJkSGrV3i3FPW_F27VVs_j0' 
 WORKSHEET_NAME = 'Ad Scraper'                   
 CREDENTIALS_FILE = 'creds.json'             
 
